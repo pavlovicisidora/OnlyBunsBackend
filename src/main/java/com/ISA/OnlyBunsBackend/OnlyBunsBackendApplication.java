c41e2041cc0bf991b92c45cb0ef6a8c0e7be0898
Isidora Pavlovic <isidora.pavlovic123@gmail.com>
2024-10-18 14:01:27 +0200

    Initial commit

---
+++ src/main/java/com/ISA/OnlyBunsBackend/OnlyBunsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ISA.OnlyBunsBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlyBunsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlyBunsBackendApplication.class, args);
+	}
+
+}
