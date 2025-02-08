a70a873f7f7543ef6059a8b8a10a7340545251de
Srxx42 <srdjan.vukojevic002@gmail.com>
2025-02-08 18:11:52 +0100

    Dodato par slika. Dodata i metoda za logout.

--- src/main/java/com/ISA/OnlyBunsBackend/controller/UserController.java
+++ src/main/java/com/ISA/OnlyBunsBackend/controller/UserController.java
@@ -100,4 +100,15 @@ public class UserController {
         List<UsersViewDTO> topUsers = userService.getTop10UsersWhoSharedMostLikesInLast7Days();
         return ResponseEntity.ok(topUsers);
     }
+
+    @PutMapping("/logout")
+    public ResponseEntity<Void> logout(){
+
+        //Ova metoda ne radi nista, koristim je samo da preko Prometheusa mogu da zabelezim
+        //koliko je korisnika online. (br poziva logina - br poziva logouta).
+        //Ako neko zeli da je iskoristi slobodno, samo ako promenite putanju ili dobate neku
+        //PathVariable, onda javite.
+
+        return ResponseEntity.ok().build();
+    }
 }
